793e8abee6b4148025543fa2ccad2c42d67a08b1
YuYongjin <radio020112@gmail.com>
2024-01-29 22:11:13 +0900

    add: GameManager Script Test Code

--- Project-YA/Assets/Script/GameManager.cs
+++ Project-YA/Assets/Script/GameManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 아직 컨셉이 명확하지 않으므로 제외
+    //public int hpForce;
+    public float attackForce;
+    public float timer;
+
+    public float enemyHP;
+    // Enemy 가상 체력
+
+    public GameObject testOBJ;
+
     void Start()
     {
-        
+        attackForce = 0f;
+        timer = 1f;
+        enemyHP = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AttackTimer();
+        FixedAttack();
+
+        Debug.Log(timer);
+    }
+
+    public void AttackTimer()
+    {
+        timer -= Time.deltaTime;
+    }
+
+    public void FixedAttack()
+    {
+        if (timer <= 0f)
+        {
+            enemyHP -= 10;
+            timer = 1f;
+            Debug.Log("공격");
+            testOBJ.SetActive(false);
+        }
     }
 }
